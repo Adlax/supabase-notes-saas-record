e82edad1d41bbd307493d04d76c6369c50ed15fe
adlax <h.videlier@gmail.com>
2026-08-27 10:31:54 +0200

    Setup tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
 	return (
 		<main>
-			<h1>Note mate</h1>
+			<h1 className="text-4xl font-bold">Note mate</h1>
 		</main>
 	);
 }
