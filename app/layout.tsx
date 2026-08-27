f98c2b9b05c82aabb8ba56f349c6ebf7d89bd0fd
adlax <h.videlier@gmail.com>
2026-08-27 14:07:13 +0200

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
+const inter = Inter({
+	variable: "--font-inter",
 	subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+	variable: "--font-roboto-mono",
 	subsets: ["latin"],
 });
 
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
 	return (
-		<html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+		<html lang="en" className={`${inter.variable} ${robotoMono.variable}`}>
 			<body>{children}</body>
 		</html>
 	);
