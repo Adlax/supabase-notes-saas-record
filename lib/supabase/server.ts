b5dc0768b037f2f2c5218129087e000d720c325c
adlax <h.videlier@gmail.com>
2026-08-29 11:51:21 +0200

    cookies config next/supa

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,12 +1,23 @@
 import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-export function createServerSupabaseClient() {
+export async function createServerSupabaseClient() {
+	const cookieStore = await cookies();
+
 	return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!, {
 		cookies: {
 			getAll() {
-				return [];
+				return cookieStore.getAll();
 			},
-			setAll() {},
+			setAll(cookiesToSet, _headers) {
+				try {
+					cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+				} catch {
+					// The `setAll` method was called from a Server Component.
+					// This can be ignored if you have middleware refreshing
+					// user sessions.
+				}
+			},
 		},
 	});
 }
