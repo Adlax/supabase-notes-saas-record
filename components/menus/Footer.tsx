89a6e5a5dd074a48bdd3696bdeed5fa39a84966b
adlax <h.videlier@gmail.com>
2026-08-31 14:32:35 +0200

    footer

--- components/menus/Footer.tsx
+++ components/menus/Footer.tsx
@@ -1,4 +1,12 @@
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className="w-full h-40 bg-[var(--userbar-bg)] flex items-center text-[var(--primary-50)] text-center">
+			<div className="mx-auto max-w-6xl flex-col items-center justify-center">
+				<div className="text-[var(--primary-400)]">Note Mate</div>
+				<div>Made by Adlax for educational purposes</div>
+				<div className="text-[var(--primary-100)]">{new Date().getFullYear()}</div>
+			</div>
+		</div>
+	);
 }
 export default Footer;
