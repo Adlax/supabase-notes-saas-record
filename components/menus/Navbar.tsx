6c08ca2bbac92e9c8795315ca74157d1f71d1c2d
adlax <h.videlier@gmail.com>
2026-08-31 14:19:25 +0200

    navbar

--- components/menus/Navbar.tsx
+++ components/menus/Navbar.tsx
@@ -1,4 +1,19 @@
+import Link from "next/link";
+
 function Navbar() {
-	return <div>Navbar</div>;
+	return (
+		<div className="border-b bg-[var(--navbar-bg)] shadow-2xl">
+			<div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-6">
+				<div className="flex items-center h-full">
+					<Link href={"/dashboard"} className="font-bold h-full hover:bg-[var(--primary-500)] transition flex items-center px-2">
+						Dashboard
+					</Link>
+					<Link href={"/about"} className="font-bold h-full hover:bg-[var(--primary-500)] transition flex items-center px-2">
+						About
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Navbar;
