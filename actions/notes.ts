c95cd0127837816f59773fc070eadf3bee0bfb46
adlax <h.videlier@gmail.com>
2026-09-01 13:57:53 +0200

    Update note action

--- actions/notes.ts
+++ actions/notes.ts
@@ -32,3 +32,35 @@ export async function createNoteAction(formData: FormData) {
 		console.log(error);
 	}
 }
+
+export async function updateNoteAction(formData: FormData) {
+	try {
+		const user = await getCurrentUser();
+
+		if (!user) {
+			throw new Error("User not logged in");
+		}
+
+		const supabase = await createServerSupabaseClient();
+
+		const id = formData.get("id") as string;
+		const title = formData.get("title") as string;
+		const content = formData.get("content") as string;
+
+		//File handling
+
+		//Note in db handling
+		const { error: dbError } = await supabase.from("notes").update({ title, content }).eq("id", id).eq("user_id", user.id);
+
+		if (dbError) {
+			throw dbError;
+		}
+
+		revalidatePath(`/notes/${id}`);
+
+		revalidatePath("/dashboard");
+	} catch (error) {
+		console.error("Update of note failed", error);
+		console.log(error);
+	}
+}
