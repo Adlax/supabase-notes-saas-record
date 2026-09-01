c8b45f9ac4fbf4c39832a7d0620ae12de1383171
adlax <h.videlier@gmail.com>
2026-09-01 09:21:19 +0200

    notes list

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { logoutAction } from "@/actions/auth";
 import { getCurrentUser } from "@/app/auth/auth";
 import Intro from "@/components/menus/Intro";
+import NotesList from "@/components/notes/NotesList";
+import { createServerSupabaseClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
 async function DashboardPage() {
@@ -10,6 +12,15 @@ async function DashboardPage() {
 		redirect("/login");
 	}
 
+	// fetch the notes
+	const supabase = await createServerSupabaseClient();
+
+	const { data: notes, error } = await supabase.from("notes").select("*").order("created_at", { ascending: false });
+
+	if (error) {
+		throw new Error(error.message);
+	}
+
 	return (
 		<div className="p-8">
 			{/* Dashboard Intro */}
@@ -17,6 +28,7 @@ async function DashboardPage() {
 			{/* Listener to build/inject session into websocket for realtime */}
 			{/* Creation form */}
 			{/* Notes Listing  */}
+			<NotesList notes={notes} />
 		</div>
 	);
 }
---
+++ components/notes/NotesList.tsx
@@ -0,0 +1,22 @@
+export type Note = {
+	id: string;
+	title: string;
+	content?: string | null;
+	imageUrl?: string | null;
+};
+
+function NotesList({ notes }: { notes: Note[] }) {
+	return (
+		<div className="mt-8">
+			<h1 className="text-2xl font-bold">My previous notes</h1>
+			{notes?.length === 0 && <p className="text-gray-500">No notes yet</p>}
+			<div className="flex flex-col gap-y-6">
+				{notes?.map((note) => (
+					<div key={note.id}>{note.title}</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default NotesList;
