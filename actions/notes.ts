f20abb24775dbb3fa6f36d9e0d883894de8c8dde
adlax <h.videlier@gmail.com>
2026-09-01 09:50:05 +0200

    Create note action

---
+++ actions/notes.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { getCurrentUser } from "@/app/auth/auth";
+import { createServerSupabaseClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export async function createNoteAction(formData: FormData) {
+	try {
+		const user = await getCurrentUser();
+
+		if (!user) {
+			throw new Error("User not logged in");
+		}
+
+		const title = formData.get("title") as string;
+
+		const supabase = await createServerSupabaseClient();
+
+		const { error } = await supabase.from("notes").insert({
+			title,
+			content: "",
+			user_id: user.id,
+		});
+
+		if (error) {
+			throw error;
+		}
+
+		revalidatePath("/dashboard");
+	} catch (error) {
+		console.error("Creation of note failed", error);
+		console.log(error);
+	}
+}
