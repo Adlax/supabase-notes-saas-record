1ca3a781e37d4b27fd88ba95c381cf464a0deb2d
adlax <h.videlier@gmail.com>
2026-09-01 14:18:14 +0200

    Note single page

---
+++ app/(private)/notes/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { getCurrentUser } from "@/app/auth/auth";
+import { createServerSupabaseClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+async function NotePage({ params }: { params: Promise<{ id: string }> }) {
+	const user = await getCurrentUser();
+
+	if (!user) {
+		redirect("/login");
+	}
+
+	const { id } = await params;
+
+	const supabase = await createServerSupabaseClient();
+
+	const { data: note } = await supabase.from("notes").select("*").eq("id", id).single();
+
+	return (
+		<div className="mx-auto max-w-3xl px-1 py-8 sm:px-8">
+			<h1 className="mb-8 text-2xl font-bold">Update the note</h1>
+			<div>No image</div>
+			{/* UpdateNoteForm */}
+		</div>
+	);
+}
+
+export default NotePage;
