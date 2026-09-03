e44365abc88db94ad6ec0a2531d64951eb83b6ab
adlax <h.videlier@gmail.com>
2026-09-03 10:22:44 +0200

    Note card

--- components/notes/NoteCard.tsx
+++ components/notes/NoteCard.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Note } from "./NotesList";
 import { deleteNoteAction } from "@/actions/notes";
 import { Trash2 } from "lucide-react";
+import Image from "next/image";
 
 function NoteCard({ note }: { note: Note }) {
 	return (
@@ -9,7 +10,13 @@ function NoteCard({ note }: { note: Note }) {
 			<div className="grid grid-cols-[1fr_auto] items-center">
 				{/* Link */}
 				<Link href={`/notes/${note.id}`} className="grid grid-cols-[80px_1fr_auto] gap-4 font-medium items-center">
-					<div className="h-20 w-20 rounded border flex items-center justify-center text-xs">No image</div>
+					{note.imageUrl ? (
+						<div className="relative h-20 w-20 overflow-hidden">
+							<Image src={note.imageUrl} alt="cover-image" fill className="object-cover" />
+						</div>
+					) : (
+						<div className="h-20 w-20 rounded border flex items-center justify-center text-xs">No image</div>
+					)}
 					<div className="overflow-hidden">
 						<h3 className="font-medium line-clamp-1">{note.title}</h3>
 						<p className="text-sm text-gray-500 line-clamp-3">{note.content}</p>
