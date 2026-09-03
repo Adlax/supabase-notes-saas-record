2cc4b72bca49cde7459081d6982e68c26f694731
adlax <h.videlier@gmail.com>
2026-09-03 14:56:41 +0200

    update note action with image handling

--- actions/notes.ts
+++ actions/notes.ts
@@ -4,6 +4,12 @@ import { getCurrentUser } from "@/app/auth/auth";
 import { createServerSupabaseClient } from "@/lib/supabase/server";
 import { revalidatePath } from "next/cache";
 
+export type UploadNoteData = {
+	title: string;
+	content: string;
+	cover_image_path?: string;
+};
+
 export async function createNoteAction(formData: FormData) {
 	try {
 		const user = await getCurrentUser();
@@ -46,11 +52,49 @@ export async function updateNoteAction(formData: FormData) {
 		const id = formData.get("id") as string;
 		const title = formData.get("title") as string;
 		const content = formData.get("content") as string;
+		const file = formData.get("cover") as File | null;
+
+		const updateNote: UploadNoteData = { title, content };
 
 		//File handling
+		if (file && file.size > 0) {
+			//Log is sent
+			console.log({ name: file.name, size: file.size, type: file.type });
+			//Check if already image and remove existing
+			const { data: existingNote, error: existingError } = await supabase
+				.from("notes")
+				.select("cover_image_path")
+				.eq("id", id)
+				.eq("user_id", user.id)
+				.single();
+			if (existingError) {
+				throw existingError;
+			}
+			if (existingNote.cover_image_path) {
+				const { error: removalError } = await supabase.storage.from("note-covers").remove([existingNote.cover_image_path]);
+				if (removalError) {
+					console.log(removalError);
+				}
+			}
+			//Check type
+			if (!file.type.startsWith("image/")) {
+				throw new Error("Wrong type");
+			}
+			//Check size
+			if (file.size > 5 * 1024 * 1024) {
+				throw new Error("File too big");
+			}
+			//If all good : upload new image
+			const filePath = `${user.id}/${id}/${Date.now()}-${file.name}`;
+			const { error: uploadError } = await supabase.storage.from("note-covers").upload(filePath, file, { upsert: true });
+			if (uploadError) {
+				throw uploadError;
+			}
+			updateNote.cover_image_path = filePath;
+		}
 
 		//Note in db handling
-		const { error: dbError } = await supabase.from("notes").update({ title, content }).eq("id", id).eq("user_id", user.id);
+		const { error: dbError } = await supabase.from("notes").update(updateNote).eq("id", id).eq("user_id", user.id);
 
 		if (dbError) {
 			throw dbError;
