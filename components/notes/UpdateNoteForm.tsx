7ed047800494ccb803ec9e8d20c411cd0526b56c
adlax <h.videlier@gmail.com>
2026-09-03 10:51:54 +0200

    update note form

--- components/notes/UpdateNoteForm.tsx
+++ components/notes/UpdateNoteForm.tsx
@@ -4,10 +4,12 @@ import { updateNoteAction } from "@/actions/notes";
 import { Note } from "./NotesList";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 function UpdateNoteForm({ note }: { note: Note }) {
 	const router = useRouter();
 	const [updated, setUpdated] = useState(false);
+	const [preview, setPreview] = useState(note.imageUrl ?? null);
 
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -26,9 +28,11 @@ function UpdateNoteForm({ note }: { note: Note }) {
 			<textarea name="content" id="content" defaultValue={note.content ?? ""} rows={10} className="w-full rounded border p-3"></textarea>
 			<div className="grid grid-cols-1 md:grid-cols-[1fr_200px] gap-4 items-center">
 				<input type="file" accept="image/*" name="cover" className="w-full rounded px-4 py-2" />
-				<div>
-					<div>No image</div>
-				</div>
+				{preview && (
+					<div className="relative w-20 h-20 overflow-hidden rounded">
+						<Image src={preview} alt="Cover preview" fill className="object-cover" />
+					</div>
+				)}
 			</div>
 			<button type="submit" className="rounded border px-4 py-2 bg-[var(--primary-200)] hover:bg-[var(--primary-500)]">
 				{updated ? "Updated!" : "Update"}
