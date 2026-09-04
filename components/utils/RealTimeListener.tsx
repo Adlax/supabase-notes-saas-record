de1c1ca3ce666d5efa8ba5fefb5e5b2bdeb3715e
adlax <h.videlier@gmail.com>
2026-09-04 12:56:09 +0200

    Realtime listener 3

--- components/utils/RealTimeListener.tsx
+++ components/utils/RealTimeListener.tsx
@@ -9,25 +9,42 @@ function RealTimeListener() {
 
 	useEffect(() => {
 		const supabase = createBrowserSupabaseClient();
-		// Channel
-		const channel = supabase
-			.channel(`notes-changes`)
-			.on(
-				"postgres_changes",
-				{
-					event: "*",
-					schema: "public",
-					table: "notes",
-				},
-				(payload) => {
-					console.log("Events: ", payload);
-					router.refresh();
-				},
-			)
-			.subscribe((status) => console.log("Realtime Status:", status));
+		//Channel type
+		let channel: ReturnType<typeof supabase.channel> | null = null;
+		//To avoid 2 times useeffect because of strict mode use this boolean:
+		let isMounted = true;
+		//Setup definition
+		async function setup() {
+			const { data } = await supabase.auth.getSession();
+			if (!data.session || !isMounted) {
+				return;
+			}
+			await supabase.realtime.setAuth(data.session.access_token);
+			// Channel
+			channel = supabase
+				.channel(`notes-changes`)
+				.on(
+					"postgres_changes",
+					{
+						event: "*",
+						schema: "public",
+						table: "notes",
+					},
+					(payload) => {
+						console.log("Events: ", payload);
+						router.refresh();
+					},
+				)
+				.subscribe((status) => console.log("Realtime Status:", status));
+		}
+		// Launch setup
+		setup();
 		//Cleaner
 		return () => {
-			supabase.removeChannel(channel);
+			isMounted = false;
+			if (channel) {
+				supabase.removeChannel(channel);
+			}
 		};
 	}, [router]);
 
